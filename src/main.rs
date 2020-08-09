5b9fff9d386a8b02b63a6c2718cbe6e8a18bddcb
Kyle Melton <kalm42@gmail.com>
2020-08-08 20:48:15 -0600

    refactor: player struct

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,17 @@ use sdl2::rect::{Point, Rect};
 use sdl2::render::{Texture, WindowCanvas};
 use std::time::Duration;
 
+#[derive(Debug)]
+struct Player {
+    position: Point,
+    sprite: Rect,
+}
+
 fn render(
     canvas: &mut WindowCanvas,
     color: Color,
     texture: &Texture,
-    position: Point,
-    sprite: Rect,
+    player: &Player,
 ) -> Result<(), String> {
     canvas.set_draw_color(color);
     canvas.clear();
@@ -19,10 +24,14 @@ fn render(
     let (width, height) = canvas.output_size()?;
 
     // 0, 0 is the center of the screen
-    let screen_position = position + Point::new(width as i32 / 2, height as i32 / 2);
-    let screen_rect = Rect::from_center(screen_position, sprite.width(), sprite.height());
+    let screen_position = player.position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(
+        screen_position,
+        player.sprite.width(),
+        player.sprite.height(),
+    );
 
-    canvas.copy(texture, sprite, screen_rect)?;
+    canvas.copy(texture, player.sprite, screen_rect)?;
     canvas.present();
 
     Ok(())
@@ -47,8 +56,10 @@ fn main() -> Result<(), String> {
     let texture_creator = canvas.texture_creator();
     let texture = texture_creator.load_texture("assets/bardo.png")?;
 
-    let position = Point::new(0, 0);
-    let sprite = Rect::new(0, 0, 26, 36);
+    let player = Player {
+        position: Point::new(0, 0),
+        sprite: Rect::new(0, 0, 26, 36),
+    };
 
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
@@ -70,13 +81,7 @@ fn main() -> Result<(), String> {
         i = (i + 1) % 255;
 
         // Render
-        render(
-            &mut canvas,
-            Color::RGB(i, 64, 255 - i),
-            &texture,
-            position,
-            sprite,
-        )?;
+        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, &player)?;
 
         // Time management!
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
