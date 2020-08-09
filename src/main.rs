19e105780b95f181ed75a2b93c04ce286e26b379
Kyle Melton <kalm42@gmail.com>
2020-08-08 21:35:04 -0600

    smooth movement

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,22 @@ use sdl2::rect::{Point, Rect};
 use sdl2::render::{Texture, WindowCanvas};
 use std::time::Duration;
 
+const PLAYER_MOVEMENT_SPEED: i32 = 20;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
 #[derive(Debug)]
 struct Player {
     position: Point,
     sprite: Rect,
     speed: i32,
+    direction: Direction,
 }
 
 fn render(
@@ -38,6 +49,24 @@ fn render(
     Ok(())
 }
 
+fn update_player(player: &mut Player) {
+    use self::Direction::*;
+    match player.direction {
+        Left => {
+            player.position = player.position.offset(-player.speed, 0);
+        }
+        Right => {
+            player.position = player.position.offset(player.speed, 0);
+        }
+        Up => {
+            player.position = player.position.offset(0, -player.speed);
+        }
+        Down => {
+            player.position = player.position.offset(0, player.speed);
+        }
+    }
+}
+
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
@@ -61,6 +90,7 @@ fn main() -> Result<(), String> {
         position: Point::new(0, 0),
         sprite: Rect::new(0, 0, 26, 36),
         speed: 5,
+        direction: Direction::Right,
     };
 
     let mut event_pump = sdl_context.event_pump()?;
@@ -80,37 +110,64 @@ fn main() -> Result<(), String> {
                     keycode: Some(Keycode::Left),
                     ..
                 } => {
-                    player.position = player.position.offset(-player.speed, 0);
+                    player.speed = PLAYER_MOVEMENT_SPEED;
+                    player.direction = Direction::Left;
                 }
                 Event::KeyDown {
                     keycode: Some(Keycode::Right),
                     ..
                 } => {
-                    player.position = player.position.offset(player.speed, 0);
+                    player.speed = PLAYER_MOVEMENT_SPEED;
+                    player.direction = Direction::Right;
                 }
                 Event::KeyDown {
                     keycode: Some(Keycode::Up),
                     ..
                 } => {
-                    player.position = player.position.offset(0, -player.speed);
+                    player.speed = PLAYER_MOVEMENT_SPEED;
+                    player.direction = Direction::Up;
                 }
                 Event::KeyDown {
                     keycode: Some(Keycode::Down),
                     ..
                 } => {
-                    player.position = player.position.offset(0, player.speed);
+                    player.speed = PLAYER_MOVEMENT_SPEED;
+                    player.direction = Direction::Down;
+                }
+                Event::KeyUp {
+                    keycode: Some(Keycode::Left),
+                    repeat: false,
+                    ..
+                }
+                | Event::KeyUp {
+                    keycode: Some(Keycode::Right),
+                    repeat: false,
+                    ..
+                }
+                | Event::KeyUp {
+                    keycode: Some(Keycode::Up),
+                    repeat: false,
+                    ..
+                }
+                | Event::KeyUp {
+                    keycode: Some(Keycode::Down),
+                    repeat: false,
+                    ..
+                } => {
+                    player.speed = 0;
                 }
                 _ => {}
             }
         }
         // Update
         i = (i + 1) % 255;
+        update_player(&mut player);
 
         // Render
         render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, &player)?;
 
         // Time management!
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 20));
     }
 
     Ok(())
